84cdc6e08ff443e36b37c796b1d48cf2c1a22267
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2022-10-10 02:23:51 +0100

    Fixed resetting of the theme color mode when reloading the page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HelmetProvider, Helmet } from 'react-helmet-async';
 import { ThemeProvider } from '@mui/material/styles';
@@ -13,19 +13,23 @@ const App = () => {
     const [mode, setMode] = useState('dark');
     const colorMode = useMemo(
         () => ({
-            // The light mode switch will invoke this method
+            // The theme mode switch will invoke this method
             toggleColorMode: () => {
+                window.localStorage.setItem('themeMode', mode === 'dark' ? 'light' : 'dark');
                 setMode((prevMode) => (prevMode === 'dark' ? 'light' : 'dark'));
-
-                try {
-                    window.localStorage.setItem('themeMode', mode);
-                } catch {
-                    /* do nothing */
-                }
             },
         }), 
-        [],
+        [mode],
     );
+
+    useEffect(() => {
+        try {
+            const localTheme = window.localStorage.getItem('themeMode');
+            localTheme ? setMode(localTheme) : setMode('dark');
+        } catch {
+            setMode('dark');
+        }
+    }, []);
 
     return (
         <HelmetProvider>
