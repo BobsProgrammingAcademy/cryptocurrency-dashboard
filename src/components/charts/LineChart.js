4f45ec70a9ce01f69a76817b3c3a381fdba09394
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2024-10-07 21:39:00 +0200

    Code refactoring

--- src/components/charts/LineChart.js
+++ src/components/charts/LineChart.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import numeral from 'numeral';
 import { Chart, registerables } from 'chart.js';
